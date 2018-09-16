438634ef983aea39e8d071249b6f649c147acce3
Ralakus <12schneide@gmail.com>
2018-09-15 20:47:43 -0400

    Added define macro

--- script.lua
+++ script.lua
@@ -55,13 +55,18 @@ function PreprocessData(strName, strData)
         if strMacroType == DefineMacro then
             local nDefineSeperator = string.find(strMacroValue, DefineMacroValueSeperator)
             if nDefineSeperator then
-                Defines[string.sub(strMacroValue, 1, nDefineSeperator - 1)] = { string.sub(strMacroValue, nDefineSeperator), nStart }
+                Defines[string.sub(strMacroValue, 1, nDefineSeperator - 1)] = { string.sub(strMacroValue, nDefineSeperator + 1), nStart }
                 print("Defined: " .. string.sub(strMacroValue, 1, nDefineSeperator - 1) .. " with a value of " .. string.sub(strMacroValue, nDefineSeperator + 1) .. " at index " .. tostring(nStart - 1))
             else 
                 assert("Error parsing define macro!\nExpected \':\' between define and value!")
                 nLastIndex = nil
                 break
             end
+
+
+        elseif strMacroType == UndefineMacro then
+            Undefines[strMacroValue] = nStart
+            print("Undefined: " .. strMacroValue .. " at index " .. tostring(nStart - 1))
 
 
         elseif strMacroType == IncludeMacro then
@@ -76,7 +81,47 @@ function PreprocessData(strName, strData)
             end
 
 
-            Includes[tablelength(Includes)] = strFileName
+            bAlreadyIncluded = false
+
+            for k, v in pairs(Includes) do
+                if v == strFileName then
+                    bAlreadyIncluded = true
+                    print("File: \"" .. strFileName .. "\" already included!")
+                end
+            end
+
+            if not bAlreadyIncluded then
+                Includes[tablelength(Includes)] = strFileName
+
+                local f = assert(io.open(strFileName, "r"))
+                local strFileContents = f:read("*all")
+                f:close()
+
+                StringInsertFormat(strWorkingData, nStart)
+                strWorkingData = string.format(strWorkingData, strFileContents)
+            end
+
+
+        elseif strMacroType == ForceIncludeMacro then
+            local strFileName = nil
+            if string.byte(strMacroValue) == string.byte(IncludeMacroAbsolutePathIndicator) then 
+                strFileName = string.sub(strMacroValue, 2)
+                print("Force including absolute file: " .. strFileName)
+            else
+                local nLastPathID = string.find(strName, "/.-$", 1)
+                strFileName = string.sub(strName, 1, nLastPathID) .. strMacroValue
+                print("Force including relative file: " .. strFileName)
+            end
+
+            bAlreadyIncluded = false
+
+            for k, v in pairs(Includes) do
+                if v == strFileName then
+                    bAlreadyIncluded = true
+                end
+            end
+
+            if not bAlreadyIncluded then Includes[tablelength(Includes)] = strFileName end
 
             local f = assert(io.open(strFileName, "r"))
             local strFileContents = f:read("*all")
@@ -84,24 +129,42 @@ function PreprocessData(strName, strData)
 
             StringInsertFormat(strWorkingData, nStart)
             strWorkingData = string.format(strWorkingData, strFileContents)
-
-
-        elseif strMacroType == ForceIncludeMacro then
-            local strFileName = nil
-            if string.byte(strMacroValue) == string.byte(IncludeMacroAbsolutePathIndicator) then 
-                print("Force including absolute file: " .. strMacroValue)
-            else
-                print("Force including relative file: " .. strMacroValue)
-            end
-
-
-
         end
 
 
 
 
     end
+
+    print("Start define processing")
+
+    for k, v in pairs(Defines) do
+
+        print(k .. "  :  " .. v[1] .. "  :  " .. tostring(v[2]))
+
+        local nLastIndex = 1
+
+        while nLastIndex do
+            local nCurrentIndex = string.find(strWorkingData, k, nLastIndex)
+            if not nCurrentIndex then
+                nLastIndex = nil
+                print("End define processing")
+                break
+            else
+                nLastIndex = nCurrentIndex + 1
+            end
+            if not Undefines[k] then
+                strWorkingData = string.gsub(strWorkingData, k, v[1], 1)
+                print("Replaced " .. k .. " with " .. v[1])
+            elseif nCurrentIndex < Undefines[k] then
+                strWorkingData = string.gsub(strWorkingData, k, v[1], 1)
+                print("Replaced " .. k .. " with " .. v[1])
+            end
+        end
+
+    end
+
+
     
 
     return strWorkingData
