b173355f15d0d4afc718f7173c1689c422becc28
Ralakus <12schneide@gmail.com>
2018-10-08 22:15:00 -0400

    Cleaned up premake5 build script

--- premake5.lua
+++ premake5.lua
@@ -5,35 +5,31 @@ workspace "WotScript"
     language "C++"
     cppdialect "C++17"
 
-    location "build"
-    if(Is64bit) then
-        location "build64"
-    else 
-        location "build32"
-    end
-
-    if(Is64bit) then
-            architecture "x86_64"
-    else
-            architecture "x86"
-    end
-
     configurations { "Debug", "Release" }
 
     filter { "configurations:Debug" }
-            symbols "On"
-            optimize "Off"
-
+        optimize "Off"
+        symbols "On"
     filter { "configurations:Release" }
-            optimize "On"
-            symbols "Off"
+        optimize "On"
+        symbols "Off"
     filter { }
 
-    if(Is64bit) then
+    if Is64bit then
+
+        location "build64"
+
+        architecture "x86_64"
+
         targetdir ("build64/bin/%{prj.name}/%{cfg.longname}")
 
         objdir ("build64/obj/%{prj.name}/%{cfg.longname}")
     else 
+
+        location "build32"
+
+        architecture "x86"
+
         targetdir ("build32/bin/%{prj.name}/%{cfg.longname}")
 
         objdir ("build32/obj/%{prj.name}/%{cfg.longname}")
@@ -47,12 +43,14 @@ project "Scanner-Preprocessor"
 
     kind "ConsoleApp"
 
-    files { "src/**.hpp", "src/**.cpp", "src/**.inl", "src/**.h", "src/**.c"  }
+    files { "src/**.hpp", "src/**.cpp" }
 
     includedirs "./lib/module"
 
     filter "configurations:Debug"
-            defines { "DEBUG" }
+        defines { "DEBUG" }
 
     filter "configurations:Release"
-            defines { "NDEBUG" }
+        defines { "NDEBUG" }
+        
+    filter { }
